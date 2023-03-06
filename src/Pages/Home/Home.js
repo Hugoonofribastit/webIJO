6d78b1cba12e596b10032d018f8b0040b2813559
Hugo <hugo.onofribastit@gmail.com>
2023-03-06 03:29:52 -0300

    main changed without carr

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,20 +2,21 @@ import React from 'react'
 import Main from '../../Components/Main/Main'; 
 import SectionCard from '../../Components/SectionCard/SectionCard';
 import Niveles from '../../Components/Niveles/Niveles';
-import Carrousel from '../../Components/Carousel/Carousel';
+/* import Carrousel from '../../Components/Carousel/Carousel'; */
 
 
 
 
 
 const Home = () =>{
+    
 return(
 <>
     <Main/>
     <SectionCard/>
     <Niveles/>
     <SectionCard/>
-    <Carrousel/>
+   
  </>
     )
 }
